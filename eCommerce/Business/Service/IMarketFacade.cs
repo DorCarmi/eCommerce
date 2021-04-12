e4dd702f8e60d74329c69231858a77e4576be955
netanel <pratn@post.bgu.ac.il>
2021-04-12 18:55:57 +0300

    Add method to the market facde (#12)
    * Add almost all the methods
    * The resent is commented by TODO

--- eCommerce/Business/Service/IMarketFacade.cs
+++ eCommerce/Business/Service/IMarketFacade.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using eCommerce.Common;
 
 namespace eCommerce.Business.Service
@@ -51,6 +52,78 @@ namespace eCommerce.Business.Service
         /// <returns>List of match products</returns>
         public Result<IEnumerable<ItemDto>> SearchForProduct(string token, string query);
         
+        /// <summary>
+        /// Add new item to the sore
+        /// </summary>
+        /// <param name="token">Authorization token</param>
+        /// <param name="item">The new item</param>
+        /// <returns>Result of the item addition</returns>
+        public Result AddNewItemToStore(string token,  ItemDto item);
+        
+        /// <summary>
+        /// Edit the item
+        /// </summary>
+        /// <param name="token">Authorization token</param>
+        /// <param name="item">The new item</param>
+        /// <returns>Result of the edit</returns>
+        public Result EditItemAmountInStore(string token, ItemDto item);
+        
+        /// <summary>
+        /// Remove item from store
+        /// </summary>
+        /// <param name="token">Authorization token</param>
+        /// <param name="storeId">The sore id</param>
+        /// <param name="itemId">The item id</param>
+        /// <returns>Result of the product removal</returns>
+        public Result RemoveProductFromStore(string token, string storeId, string itemId);
+        
+        // TODO requirement 4.2
+
+        /// <summary>
+        /// Appoint a user as a coOwner to the store
+        /// </summary>
+        /// <param name="token">Authorization token</param>
+        /// <param name="storeId">The store id</param>
+        /// <param name="appointedUserId">The appointment user</param>
+        /// <returns>Result of the appointment</returns>
+        public Result AppointCoOwner(string token, string storeId, string appointedUserId);
+        
+        /// <summary>
+        /// Appoint a user as a new manager to the sore 
+        /// </summary>
+        /// <param name="token">Authorization token</param>
+        /// <param name="storeId">The store id</param>
+        /// <param name="appointedManagerUserId">The appointment manager</param>
+        /// <returns>Result of the appointment</returns>
+        public Result AppointManager(string token, string storeId, string appointedManagerUserId);
+        
+        // TODO how to define and send the permission
+        /// <summary>
+        /// Update the manager permission
+        /// </summary>
+        /// <param name="token">Authorization token</param>
+        /// <param name="storeId">The store id</param>
+        /// <param name="appointedManagerUserId">The User manager id</param>
+        /// <param name="permissions">The updated permission</param>
+        /// <returns>Result of the update</returns>
+        public Result UpdateManagerPermission(string token, string storeId, string appointedManagerUserId, IList<StorePermission> permissions);
+
+        /// <summary>
+        /// Get all the staff of the store and their permissions
+        /// </summary>
+        /// <param name="token">Authorization token</param>
+        /// <param name="storeId">The storeId</param>
+        /// <returns>List of all the staff and their permissions</returns>
+        public Result<IEnumerable<StaffPermission>> GetStoreStaffAndTheirPermissions(string token, string storeId);
+        
+        /// <summary>
+        /// Return all the purchase history of a store
+        /// </summary>
+        /// <param name="token">Authorization token</param>
+        /// <param name="storeId">The storeId</param>
+        /// <returns>List of the purchase history in a store</returns>
+        public Result<IEnumerable<PurchaseHistory>> GetPurchaseHistoryOfStore(string token, string storeId);
+
         // ========== User ========== //
         
         // TODO purchase option
@@ -95,5 +168,41 @@ namespace eCommerce.Business.Service
         /// <param name="token">Authorization token</param>
         /// <returns>The result the purchase</returns>
         public Result PurchaseCart(string token);
+        
+        /// <summary>
+        /// Open a new store for the user.
+        /// The name need to be unique
+        /// </summary>
+        /// <param name="token">Authorization token</param>
+        /// <param name="storeName">The store name</param>
+        /// <returns>Result of the request</returns>
+        public Result OpenStore(string token, string storeName);
+
+        /// <summary>
+        /// Get the purchase history of the user 
+        /// </summary>
+        /// <param name="token">Authorization token</param>
+        /// <returns>The purchase history</returns>
+        public Result<IEnumerable<PurchaseHistory>> GetPurchaseHistory(string token);
+        
+        // ========== Admin ========== //
+        
+        /// <summary>
+        /// Get the history purchase of a user
+        /// </summary>
+        /// <param name="token">Authorization token</param>
+        /// <param name="storeId">The store id</param>
+        /// <param name="ofUserId">The user id</param>
+        /// <returns>The history purchase</returns>
+        public Result<IEnumerable<PurchaseHistory>> AdminGetPurchaseHistoryUser(string token, string storeId, string ofUserId);
+        
+        /// <summary>
+        /// Get the history purchase of a store
+        /// </summary>
+        /// <param name="token">Authorization token</param>
+        /// <param name="storeId">The store id</param>
+        /// <returns>The history purchase</returns>
+        public Result<IEnumerable<PurchaseHistory>> AdminGetPurchaseHistoryStore(string token, string storeId);
+
     }
 }
---
+++ eCommerce/Business/Service/StaffPermission.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace eCommerce.Business.Service
+{
+    public class StaffPermission
+    {
+        public string UserId { get; set; }
+        public IEnumerable<StorePermission> Permissions { get; set; }
+    }
+}
