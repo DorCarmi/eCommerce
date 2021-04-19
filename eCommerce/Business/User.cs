3219e8d7c27dd0f8b3aabfff0f9e800b0ccb4715
magrils <elp1234l>
2021-04-19 07:46:30 +0300

    added unit tests for User. also, started writing integration tests with Store.

--- eCommerce/Business/User.cs
+++ eCommerce/Business/User.cs
@@ -13,9 +13,9 @@ namespace eCommerce.Business
         private bool _isRegistered;
         private UserToSystemState _systemState;
         private MemberInfo _memberInfo;
-        private ICart _myCart;
         public string Username {get => _memberInfo.Username;}
-        
+        private ICart _myCart;
+
         private Object dataLock;
         //MemberData:
         private ConcurrentDictionary<IStore, bool> _storesFounded;
@@ -24,8 +24,7 @@ namespace eCommerce.Business
         private ConcurrentDictionary<IStore, IList<OwnerAppointment>> _appointedOwners;
         private ConcurrentDictionary<IStore, IList<ManagerAppointment>> _appointedManagers;
         private UserTransactionHistory _transHistory ;
-
-
+        
 
         //constructors
         public User(string userName)
@@ -36,8 +35,6 @@ namespace eCommerce.Business
             _isRegistered = false;
             dataLock = new Object();
         }
-
-
         public User(MemberInfo info)
         {
             _isRegistered = true;
@@ -68,8 +65,8 @@ namespace eCommerce.Business
             _transHistory = new UserTransactionHistory();
         }
 
-
-        //Facade
+        
+    #region User Facacde Interface
         public Result Login(UserToSystemState systemState, MemberData memberData)
         {
             if (systemState == null)
@@ -91,73 +88,132 @@ namespace eCommerce.Business
             return _systemState.Logout(this,toGuestName);
         }
 
+        /**
+         * 
+         * @method 
+         */
         public Result<bool> IsRegistered()
         {
             return Result.Ok(_isRegistered);
         }
 
+        /// <TEST> UserTest.TestOpenStore </TEST>
+        /// <UC> 'Open a Store' </UC>
+        /// <REQ> 3.2 </REQ>
+        /// <summary>
+        ///  receives an IStore to open. makes this User a founder and an owner. 
+        /// </summary>
+        /// <param name="store"></param>
+        /// <returns>Result, OK/Fail. </returns>
         public Result OpenStore(IStore store)
         {
             return _systemState.OpenStore(this, store);
         }
-
+        
+        /// <TEST> UserTest.TestAddItemToCart </TEST>
+        /// <UC> 'Add product to basket' </UC>
+        /// <REQ> 2.7 </REQ>
+        /// <summary>
+        ///  adds the given items to the user's cart. 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns>Result, OK/Fail. </returns>
         public Result AddItemToCart(ItemInfo item)
         {
             return _myCart.AddItemToCart(this, item);
         }
 
+        /// <TEST> UserTest.TestAddItemToCart </TEST>
+        /// <UC> 'View shopping cart' </UC>
+        /// <REQ> 2.8 </REQ>
+        /// <summary>
+        ///  gets the information from the cart. 
+        /// </summary>
+        /// <returns>Result, ICart. </returns>
         public Result<ICart> GetCartInfo()
         {
             return Result.Ok<ICart>(_myCart);
         }
 
+        /// <TEST> UserTest.TestEditCart </TEST>
+        /// <UC> 'Edit shopping cart' </UC>
+        /// <REQ> 2.8 </REQ>
+        /// <summary>
+        ///  changes the given items in user's cart. 
+        /// </summary>
+        /// <param name="info"></param>
+        /// <returns>Result, OK/Fail . </returns>
         public Result EditCart(ItemInfo info)
         {
             return _myCart.EditCartItem(this, info);
         }
-
+        
         public Result AppointUserToOwner(IStore store, IUser user)
         {
             return _systemState.AppointUserToOwner(this, store, user);
         }
 
+        /// <TEST> UserTest.TestAppointUserToManager </TEST>
+        /// <UC> 'Nominate member to be store manager' </UC>
+        /// <REQ> 4.4 </REQ>
+        /// <summary>
+        ///  make the user a manager of the given store. 
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="store"></param>
+        /// <returns>Result, OK/Fail. </returns>
         public Result AppointUserToManager(IStore store, IUser user)
         {
             return _systemState.AppointUserToManager(this, store, user);
         }
-
-        // remove 2 functions:
-        public Result AddPermissionsToManager(IStore store, IUser user, StorePermission permission)
-        {
-            return _systemState.AddPermissionsToManager(this, store, user, permission);
-        }
-
-        public Result RemovePermissionsToManager(IStore store, IUser user, StorePermission permission)
-        {
-            return _systemState.RemovePermissionsToManager(this, store, user, permission);
-        }
-
+        
+        /// <TEST> UserTest.TestUpdatePermissionsToManager </TEST>
+        /// <UC> 'Change management permission for sub-manger' </UC>
+        /// <REQ> 4.6 </REQ>
+        /// <summary>
+        ///  change manager's permissions in the given store. 
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="store"></param>
+        /// <param name="permissions"></param>
+        /// <returns>Result, OK/Fail. </returns>
         public Result UpdatePermissionsToManager(IStore store, IUser user, IList<StorePermission> permissions)
         {
             return _systemState.UpdatePermissionsToManager(this, store, user, permissions);
         }
-
-
+        
         public Result<IList<IUser>> GetAllStoreStakeholders(IStore store)
         {
-            //@TODO: find out if this means all of apointed-by's
+            //@TODO_SHARON:: find out if this means all of apointed-by's. might need adjustments on Store side.
             throw new NotImplementedException();
         }
 
+        /// <TEST> UserTest.TestUserPurchaseHistory </TEST>
+        /// <UC> 'Review purchase history' </UC>
+        /// <REQ> 3.7 </REQ>
+        /// <summary>
+        ///  returns all purchase-records of the user. 
+        /// </summary>
         public Result<IList<PurchaseRecord>> GetUserPurchaseHistory()
         {
             return _systemState.GetUserPurchaseHistory(this);
         }
+        /// <TEST> UserTest.TestAdminGetHistory </TEST>
+        /// <UC> 'Admin requests for user history' </UC>
+        /// <REQ> 6.4 </REQ>
+        /// <summary>
+        ///  returns all purchase-records of the requested user. 
+        /// </summary>
         public Result<IList<PurchaseRecord>> GetUserPurchaseHistory(IUser otherUser)
         {
             return _systemState.GetUserPurchaseHistory(this, otherUser);
         }
-
+        /// <TEST> UserTest.TestGetStoreHistory </TEST>
+        /// <UC> 'Member requests for purchase history for the store' </UC>
+        /// <REQ> 4.11 </REQ>
+        /// <summary>
+        ///  returns all purchase-records of the requested store. 
+        /// </summary>
         public Result<IList<PurchaseRecord>> GetStorePurchaseHistory(IStore store)
         {
             return _systemState.GetStorePurchaseHistory(this, store);
@@ -177,21 +233,28 @@ namespace eCommerce.Business
 
 
         //InBusiness
+        
+        
         public Result HasPermission(IStore store, StorePermission storePermission)
         {
             return _systemState.HasPermission(this, store, storePermission);
         }
 
+        /// <TEST> UserTest.TestUserPurchaseHistory </TEST>
+        /// <UC> 'Review purchase history' </UC>
+        /// <REQ> 3.7 </REQ>
+        /// <summary>
+        ///  add records to member's history. 
+        /// </summary>
         public Result EnterRecordToHistory(PurchaseRecord record)
         {
             return _systemState.EnterRecordToHistory(this, record);
         }
+    #endregion User Facade Interface
 
 
-
-        #region Admin Functions
-        //@TODO:: add required functions
-       
+    #region Admin Functions
+    //add necessary functions   
 
     #endregion Admin Functions
     
@@ -214,6 +277,7 @@ namespace eCommerce.Business
             _isRegistered = true;
             return Result.Ok();
         }
+   
     #endregion Guest Functions
     
     
@@ -233,12 +297,12 @@ namespace eCommerce.Business
         {
             // adds store to both Owned-By and Founded-By
             OwnerAppointment owner = new OwnerAppointment(this);
-            // @TODO:: add extra founder's permissions to 'owner'
+            // @TODO_SHARON:: add extra founder's permissions to 'owner'
             
             bool res =_storesFounded.TryAdd(store,true) && _storesOwned.TryAdd(store,owner);
             if (res)
             {
-                return Result.Ok();
+                return store.AppointNewOwner(this,owner);
             }
             return Result.Fail("Unable to open store");
         }
@@ -316,7 +380,7 @@ namespace eCommerce.Business
 
         public Result<ManagerAppointment> MakeManager(Member member, IStore store)
         {
-            ManagerAppointment newManager = new ManagerAppointment();
+            ManagerAppointment newManager = new ManagerAppointment(this);
             if (!_storesOwned.ContainsKey(store) && _storesManaged.TryAdd(store, newManager))
             {
                 return Result.Ok<ManagerAppointment>(newManager);
@@ -373,9 +437,13 @@ namespace eCommerce.Business
             return Result.Fail("user\'"+Username+"\' can not remove permissions from given manager");
         }
 
-        
-        public Result UpdatePermissionsToManager(Member member, IStore store, IUser otherUser, IList<StorePermission> permissions)
+
+        public Result UpdatePermissionsToManager(Member member, IStore store, IUser otherUser,
+            IList<StorePermission> permissions)
         {
+            if (permissions == null || permissions.Count == 0){
+                return Result.Fail("Invalid permission list input");
+            }
             if (_appointedManagers.ContainsKey(store))
             {
                 ManagerAppointment manager = null;
@@ -423,7 +491,19 @@ namespace eCommerce.Business
             return store.GetPurchaseHistory(this);
         }
         
-        #endregion //Member Functions
+    #endregion //Member Functions
+        
+    
+    #region Test Oriented Functions
+
+    public ConcurrentDictionary<IStore, IList<ManagerAppointment>> AppointedManagers => _appointedManagers;
+    public ConcurrentDictionary<IStore, IList<OwnerAppointment>> AppointedOwners => _appointedOwners;
+    public ConcurrentDictionary<IStore, ManagerAppointment> StoresManaged => _storesManaged;
+    public ConcurrentDictionary<IStore, OwnerAppointment> StoresOwned => _storesOwned;
+
+    
+    #endregion
+    
     }
     
    }
