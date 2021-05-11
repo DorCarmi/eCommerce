b195545ac487dc4776247b3f0a956ac241a31082
netanelpr <pratn@post.bgu.ac.il>
2021-05-11 08:29:01 +0300

    Add simple sidebar (#64)

---
+++ eCommerce/ClientApp/src/components/SideBarMenu.jsx
@@ -0,0 +1,45 @@
+﻿import React, {Component} from "react";
+import {Menu, MenuItem, ProSidebar, SidebarContent, SidebarHeader} from "react-pro-sidebar";
+import "./SideBarMenu.css"
+
+export class SideBarMenu extends Component {
+    static displayName = SideBarMenu.name;
+
+    render() {
+        return (
+            <ProSidebar
+            breakPoint="md"
+            style={{
+                height: '100%',
+                width: '270px',
+                backgroundColor: '#e1e1e1'
+            }}>
+                <SidebarHeader>
+                    <div
+                        style={{
+                            padding: '24px',
+                            textTransform: 'uppercase',
+                            fontWeight: 'bold',
+                            fontSize: 14,
+                            letterSpacing: '1px',
+                            overflow: 'hidden',
+                            textOverflow: 'ellipsis',
+                            whiteSpace: 'nowrap',
+                        }}
+                    >
+                        "hello"
+                    </div>
+                </SidebarHeader>
+    
+                <SidebarContent>
+                    <Menu iconShape="circle">
+                        <MenuItem
+                            suffix={<span className="badge red">A</span>}
+                        >
+                            dashboard
+                        </MenuItem>
+                    </Menu>
+                </SidebarContent>
+        </ProSidebar>)
+    }
+}
