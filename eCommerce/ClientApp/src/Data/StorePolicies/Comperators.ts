ced7d45750ca56291aba1c3f2f0ad1e0663d568a
netanel <pratn@post.bgu.ac.il>
2021-05-26 16:01:30 +0300

    Add rules for discount (#77)

---
+++ eCommerce/ClientApp/src/Data/StorePolicies/Comperators.ts
@@ -0,0 +1,9 @@
+﻿export enum Comperators
+{
+    BIGGER,
+    BIGGER_EQUALS,
+    SMALLER,
+    SMALLER_EQUALS,
+    EQUALS,
+    NOT_EQUALS
+}
---
+++ eCommerce/ClientApp/src/Data/StorePolicies/DiscountInfoTree.ts
@@ -0,0 +1,18 @@
+﻿export enum DiscountNodeType {
+    Leaf,
+    Composite,
+}
+
+export type RuleNode = DiscountNodeLeaf
+
+export type DiscountNodeLeaf = {
+    type: DiscountNodeType,
+    rule: RuleNode
+}
+
+export function makeRuleNodeLeaf(rule: RuleNode): DiscountNodeLeaf {
+    return {
+        type: DiscountNodeType.Leaf,
+        rule: rule
+    }
+}
