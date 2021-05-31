0a6f472139ee9737fa029513af9ea11c5dd5e956
magrils <elp1234l>
2021-05-31 17:24:01 +0300

    sql server functional

--- eCommerce/Program.cs
+++ eCommerce/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using eCommerce.Business;
 using eCommerce.DataLayer;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -13,8 +14,8 @@ namespace eCommerce
         public static void Main(string[] args)
         {
             // CreateHostBuilder(args).Build().Run();
-            test_write();
-            // test_read();
+            // test_write();
+            test_read();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -26,41 +27,54 @@ namespace eCommerce
             using (var db = new testContext())
             {
                 var student1 = new Student() {StudentId = 1, StudentName = "One"};
-                // var course1 = new Course() {CourseId = 8, CourseName = "history"};
+                var course1 = new Course() {CourseId = 8, CourseName = "history"};
                 var student2 = new Student() {StudentId = 2, StudentName = "Two"};
-                // var course2 = new Course() {CourseId = 9, CourseName = "Arts"};
-                // var class1 = new Classroom() {ClassroomId = 43};
-                // var class2 = new Classroom() {ClassroomId = 45};
-                // var pair1_1 = new MyPair<Classroom, Course>()
-                // {
-                //     StudentId = student1.StudentId, Courses =  new List<Course>(){course1}, ClassroomId = class1.ClassroomId,
-                //     Classroom = class1
-                // };
+                var course2 = new Course() {CourseId = 9, CourseName = "Arts"};
+                var class1 = new Classroom() {ClassroomId = 43};
+                var class2 = new Classroom() {ClassroomId = 45};
+                var pair1_1 = new MyPair<Classroom, Course>()
+                {
+                    StudentId = student1.StudentId, Courses =  new List<Course>(){course1}, ClassroomId = class1.ClassroomId,
+                    Classroom = class1
+                };
                 
-                // var pair1_2 = new MyPair<Classroom, Course>()
-                // {
-                //     StudentId = student1.StudentId, Courses =  new List<Course>(){course2}, ClassroomId = class2.ClassroomId,
-                //     Classroom = class2
-                // };
-                // student1.dict.Add(pair1_1);
-                // student1.dict.Add(pair1_2);
-                // var pair2_1 = new MyPair<Classroom, Course>()
-                // {
-                //     StudentId = student2.StudentId, Courses =  new List<Course>(){course1}, ClassroomId = class1.ClassroomId,
-                //     Classroom = class1
-                // };
-                //
-                // var pair2_2 = new MyPair<Classroom, Course>()
-                // {
-                //     StudentId = student2.StudentId, Courses =  new List<Course>(){course1,course2}, ClassroomId = class2.ClassroomId,
-                //     Classroom = class2
-                // };
-                // student2.dict.Add(pair2_1);
-                // student2.dict.Add(pair2_2);
+                var pair1_2 = new MyPair<Classroom, Course>()
+                {
+                    StudentId = student1.StudentId, Courses =  new List<Course>(){course2}, ClassroomId = class2.ClassroomId,
+                    Classroom = class2
+                };
+                student1.dict.Add(pair1_1);
+                student1.dict.Add(pair1_2);
+                var pair2_1 = new MyPair<Classroom, Course>()
+                {
+                    StudentId = student2.StudentId, Courses =  new List<Course>(){course1}, ClassroomId = class1.ClassroomId,
+                    Classroom = class1
+                };
+                
+                var pair2_2 = new MyPair<Classroom, Course>()
+                {
+                    StudentId = student2.StudentId, Courses =  new List<Course>(){course1,course2}, ClassroomId = class2.ClassroomId,
+                    Classroom = class2
+                };
+                student2.dict.Add(pair2_1);
+                student2.dict.Add(pair2_2);
                 // Create
                 Console.WriteLine("Inserting a new blog");
                 db.Add(student1);
                 db.Add(student2);
+                db.SaveChanges();
+                
+            }
+        }
+ private static void test_write2()
+        {
+            using (var db = new ECommerceContext())
+            {
+                
+                var ja = new User(new MemberInfo("Ja Morant", "ja@mail.com", "Ja", DateTime.Now, "Memphis"));
+                // Create
+                Console.WriteLine("Inserting a new User");
+                db.Add(ja);
                 db.SaveChanges();
                 
             }
@@ -82,7 +96,10 @@ namespace eCommerce
                      .ThenInclude(par => par.Classroom)
                      .OrderBy(s => s.StudentId)
                      .ToList();
-                 Console.WriteLine(students[0].dict[0].Courses[0] == students[1].dict[0].Courses[0]);
+                 
+                 // var students = db.Students.ToList();
+
+                 Console.WriteLine(students[0]);
              }
         }
 
