bf6d73bbcd7ecb34028f45dfce46cc255136b11c
netanel <pratn@post.bgu.ac.il>
2021-06-11 13:14:52 +0300

    Update stats

---
+++ eCommerce/Communication/StatsHub.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using eCommerce.Common;
+using eCommerce.Publisher;
+using eCommerce.Service;
+using eCommerce.Statistics;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.SignalR;
+
+namespace eCommerce.Communication
+{
+    public class StatsMessageModel
+    {
+        public string UserType { get; set; }
+        
+        public int Amount { get; set; }
+
+        public StatsMessageModel(string userType, int amount)
+        {
+            UserType = userType;
+            Amount = amount;
+        }
+    }
+    
+    public class StatsHub : Hub, Reciver
+    {
+        private IHubContext<MessageHub> _hubContext = null;
+        
+        private IStatisticsService _statistics;
+        private IAuthService _authService;
+        public StatsHub(IHubContext<MessageHub> hubContext)
+        {
+            _hubContext = hubContext;
+            _statistics = Statistics.Statistics.GetInstance();
+            _authService = new AuthService();
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            var httpContext = Context.GetHttpContext();
+            if (httpContext == null)
+            {
+                // close connection
+                return base.OnDisconnectedAsync(null);
+            }
+
+            var authToken = httpContext.Request.Cookies["_auth"];
+            if (!_authService.IsUserConnected(authToken))
+            {
+                return base.OnDisconnectedAsync(null);
+            }
+
+            Console.WriteLine("--> Stats Connection Opened: " + Context.ConnectionId);
+            _statistics.Register(this);
+            return base.OnConnectedAsync();
+        }
+        
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            Console.WriteLine("Stats close connection");
+            _statistics.UnRegister(this);
+
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public void ReciveBrodcast(string userType, int number)
+        {
+            _hubContext.Clients.All.SendAsync("LoginUpdate", new StatsMessageModel(userType, number));
+        }
+    }
+}
